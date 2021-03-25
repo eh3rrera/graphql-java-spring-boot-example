6492319054a43f8bdb7b9d39ee4a60eb88cbab7f
Ansonator <ansonator@hotmail.com>
2021-03-25 10:35:42 -0500

    uplifted dependencies and added UIs

--- src/main/java/com/example/DemoGraphQL/repository/AuthorRepository.java
+++ src/main/java/com/example/DemoGraphQL/repository/AuthorRepository.java
@@ -1,7 +1,8 @@
 package com.example.DemoGraphQL.repository;
 
 import com.example.DemoGraphQL.model.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long>
+{
 }
--- src/main/java/com/example/DemoGraphQL/resolver/Query.java
+++ src/main/java/com/example/DemoGraphQL/resolver/Query.java
@@ -1,32 +1,39 @@
 package com.example.DemoGraphQL.resolver;
 
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.example.DemoGraphQL.model.Author;
 import com.example.DemoGraphQL.model.Book;
 import com.example.DemoGraphQL.repository.AuthorRepository;
 import com.example.DemoGraphQL.repository.BookRepository;
+import graphql.kickstart.tools.GraphQLQueryResolver;
 
-public class Query implements GraphQLQueryResolver {
+public class Query implements GraphQLQueryResolver
+{
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
 
-    public Query(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public Query(AuthorRepository authorRepository, BookRepository bookRepository)
+    {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
     }
 
-    public Iterable<Book> findAllBooks() {
+    public Iterable<Book> findAllBooks()
+    {
         return bookRepository.findAll();
     }
 
-    public Iterable<Author> findAllAuthors() {
+    public long countBooks()
+    {
+        return bookRepository.count();
+    }
+
+    public Iterable<Author> findAllAuthors()
+    {
         return authorRepository.findAll();
     }
 
-    public long countBooks() {
-        return bookRepository.count();
-    }
-    public long countAuthors() {
+    public long countAuthors()
+    {
         return authorRepository.count();
     }
 }
