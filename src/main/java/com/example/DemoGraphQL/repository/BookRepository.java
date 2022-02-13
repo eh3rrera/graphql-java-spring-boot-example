583721fa09d283702c002625b7518fcae1e6cef8
ansonator <ansonator@hotmail.com>
2022-02-12 21:18:13 -0600

    minor formatting fix

--- src/main/java/com/example/DemoGraphQL/repository/BookRepository.java
+++ src/main/java/com/example/DemoGraphQL/repository/BookRepository.java
@@ -3,6 +3,5 @@ package com.example.DemoGraphQL.repository;
 import com.example.DemoGraphQL.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long>
-{
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
